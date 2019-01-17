1e7906ff51cf0a142bf1e48fbd1cbacd0213f3ba
MISTAnalytics <46724952+MISTAnalytics@users.noreply.github.com>
2019-01-17 20:36:05 +0100

    Create sentimentSelect.js

---
+++ sentimentSelect.js
@@ -0,0 +1,9 @@
+function() {
+  
+    //Fill the CSS query selector instead of '#input_querySelector'.
+  
+	var sentimentSelect = document.querySelector("#input_querySelector").innerHTML;
+  
+	return sentimentSelect;
+  
+  }
